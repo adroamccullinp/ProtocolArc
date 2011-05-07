dcc9d7a85ec7fc64718f7ffb977c6472387dcae6
adroamccullinp <268234+adroamccullinp@users.noreply.github.com>
2011-05-07 18:52:00 +0700

    chore: field specs

---
+++ protocolarc/matrix.py
@@ -0,0 +1,9 @@
+"""Contract compatibility matrix.
+
+A matrix cross-references a set of envelopes against a set of contracts and
+records, for each pair, whether the envelope validates. It also groups
+contracts by name and builds a revision chain so the report can show how a
+protocol evolved.
+"""
+
+from __future__ import annotations
