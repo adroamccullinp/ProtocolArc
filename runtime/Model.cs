7600a65d505aaf9c9d624d70dbd4ec0f9b2fbab0
adroamccullinp <268234+adroamccullinp@users.noreply.github.com>
2011-12-16 14:31:00 +0700

    feat: dotnet runtime

--- runtime/Model.cs
+++ runtime/Model.cs
@@ -4,3 +4,11 @@ namespace ProtocolArc.Runtime;
 
 /// <summary>
 /// JSON type names recognised by the ProtocolArc runtime. These mirror the
+/// Python package so that a contract validated in either component yields the
+/// same findings.
+/// </summary>
+public static class JsonTypes
+{
+    public static readonly string[] All =
+    {
+        "string", "integer", "number", "boolean", "object", "array", "null", "any"
