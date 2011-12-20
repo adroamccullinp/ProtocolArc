b17be1ff1b5bab4fdc46110b719974f42c8bc60b
adroamccullinp <268234+adroamccullinp@users.noreply.github.com>
2011-12-20 15:56:00 +0700

    refactor: tidy message contracts

---
+++ runtime/Program.cs
@@ -0,0 +1,21 @@
+using System.Text.Json;
+using ProtocolArc.Runtime;
+
+// ProtocolArc.Runtime — a native envelope validator.
+//
+// Usage:
+//   ProtocolArc.Runtime validate <contract.json> <envelope.json|.jsonl> [--json]
+//   ProtocolArc.Runtime describe <contract.json>
+//   ProtocolArc.Runtime selfcheck
+//
+// The runtime mirrors the Python validator so that a contract validated by
+// either component yields identical findings. It is intended for hot paths
+// where envelopes stream through a gateway and low-latency checking matters.
+
+return Cli.Run(args);
+
+internal static class Cli
+{
+    public static int Run(string[] args)
+    {
+        if (args.Length == 0)
