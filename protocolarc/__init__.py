7df330f04634d18162cc26d20305ebf6247be268
adroamccullinp <268234+adroamccullinp@users.noreply.github.com>
2012-05-01 10:59:00 +0700

    test: cover protocol versioning

---
+++ protocolarc/__init__.py
@@ -0,0 +1,4 @@
+"""ProtocolArc — an agent protocol contract lab.
+
+ProtocolArc models MCP/A2A-style message envelopes as versioned contracts,
+validates instances against those contracts, compares contract revisions to
