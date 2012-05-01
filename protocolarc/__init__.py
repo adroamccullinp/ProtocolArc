16f42e3784727d5f12e2e532606d90fed04e1b95
adroamccullinp <268234+adroamccullinp@users.noreply.github.com>
2012-05-01 11:04:00 +0700

    fix: dotnet runtime

--- protocolarc/__init__.py
+++ protocolarc/__init__.py
@@ -2,3 +2,10 @@
 
 ProtocolArc models MCP/A2A-style message envelopes as versioned contracts,
 validates instances against those contracts, compares contract revisions to
+classify compatibility changes, and renders a deterministic contract matrix
+and report.
+
+Public surface:
+    load_contract          Parse a contract document into a Contract.
+    load_envelope          Parse an envelope instance into an Envelope.
+    validate_envelope      Validate an envelope against a contract.
