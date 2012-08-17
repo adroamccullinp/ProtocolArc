df3a7c40a8169198bfd166b396a9a1c32b4898f8
adroamccullinp <268234+adroamccullinp@users.noreply.github.com>
2012-08-17 16:03:00 +0700

    test: cover schema evolution

--- tests/test_protocolarc.py
+++ tests/test_protocolarc.py
@@ -2,3 +2,12 @@ import json
 import subprocess
 import sys
 from pathlib import Path
+
+ROOT = Path(__file__).resolve().parent.parent
+CLI = [sys.executable, "-m", "protocolarc"]
+
+
+def _run(*args):
+    return subprocess.run(CLI + list(args), cwd=str(ROOT), capture_output=True, text=True)
+
+
