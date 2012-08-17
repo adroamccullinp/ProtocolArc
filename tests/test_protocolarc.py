1c9a7b84336d844b130f1c8f7bc470342f1eb5dc
adroamccullinp <268234+adroamccullinp@users.noreply.github.com>
2012-08-17 16:21:00 +0700

    docs: document compatibility diff

--- tests/test_protocolarc.py
+++ tests/test_protocolarc.py
@@ -11,3 +11,11 @@ def _run(*args):
     return subprocess.run(CLI + list(args), cwd=str(ROOT), capture_output=True, text=True)
 
 
+def test_contracts_load():
+    from protocolarc.loader import load_contract
+    c100 = load_contract(ROOT / "examples" / "contracts" / "mcp_tool_call_1.0.0.json")
+    c200 = load_contract(ROOT / "examples" / "contracts" / "mcp_tool_call_2.0.0.json")
+    assert c100.revision == "1.0.0"
+    assert c200.revision == "2.0.0"
+
+
